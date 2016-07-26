3066f7d487e838acc7f04ae63876ca6fe76126cf
bgbglll <602566046@qq.com>
2016-07-26 10:25:13 +0800

    nowcoder

---
+++ leetcode/leetcode/leetcode.cpp
@@ -0,0 +1,13 @@
+// leetcode.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "LuckyStringSolution.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	LuckyStringSolution
+
+	return 0;
+}
+
