5872bd6f0eb7b45a2dd27ae943f829710b88ff83
bgbglll <602566046@qq.com>
2016-09-07 10:47:13 +0800

    leetcode 36

---
+++ leetcode/36. Valid Sudoku/36. Valid Sudoku.cpp
@@ -0,0 +1,36 @@
+// 36. Valid Sudoku.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+		unordered_set<char> col[9];
+		unordered_set<char> row[9];
+		unordered_set<char> cell[3][3];
+		for (int i = 0; i < 9; i++){
+			for (int j = 0; j < 9; j++){
+				char n = board[i][j];
+				int x = i / 3;
+				int y = j / 3;
+				if (isdigit(n) && (!row[i].insert(n).second || !col[j].insert(n).second || !cell[x][y].insert(n).second))
+					return false;
+			}
+		}
+		return true;
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
+
